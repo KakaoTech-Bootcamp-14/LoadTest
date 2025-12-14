98c908ea00ab2d894b127a7992b8486bdb0dd220
eedo_y <doup2001@naver.com>
2025-12-14 22:00:51 +0900

    ♻️refactor : 테스트 타임아웃 제거

--- e2e/actions/auth.actions.js
+++ e2e/actions/auth.actions.js
@@ -28,7 +28,6 @@ async function registerAction(page, userData) {
   await page.getByTestId('register-password-confirm-input').fill(userData.passwordConfirm);
   await page.getByTestId('register-name-input').fill(userData.name);
   await page.getByTestId('register-submit-button').click();
-  await page.waitForTimeout(1000);
 }
 
 /**
